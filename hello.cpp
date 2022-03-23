9cf92d5a047915989cbdc5b91900514b0d398716
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2022-03-23 17:29:36 +0530

    Learnt Loops

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float a=3.1415, area;
+    int r=5;
+    int exp = pow(r,15)
+    cout << "The value of pi is " << a << endl << "And the circumference is " << 2* a * r << endl << exp;
+
+    return 0;
+}
